1c05f87871b1ac5751f8adb96c986454082b5699
Oduaine Miller(Prauh) <duainem7@gmail.com>
2026-04-16 14:59:24 -0700

    initial ui establishment

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Apex from './Apex.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <div className="App-body">
+      </div>
+
+      <div className="App-footer">
+      <div><h1></h1></div>
+      <div><h5></h5></div>
+      </div>
+
+    <Apex />
+    
+        
+    </div>
+  );
+}
+
+export default App;
